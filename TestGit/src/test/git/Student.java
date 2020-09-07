943dce26b7294771c400747c1679f72ffd29cbfa
Victoria <victoria.sanchez@mail.mcgill.ca>
2020-09-06 20:00:40 -0400

    student attribute name added

--- TestGit/src/test/git/Student.java
+++ TestGit/src/test/git/Student.java
@@ -1,5 +1,19 @@
 package test.git;
 
 public class Student {
+	String name;
+	
+	public Student() {
+		
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String studentName) {
+		this.name = name;
+	}
+	
+	
 }
